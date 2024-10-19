dce16f7a426952f406724e36a26a3d70f15668fb
apnabahikhata <rohtash.poonia@apnabahikhata.com>
2024-10-19 10:15:17 +0300

    changes

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,12 +1,24 @@
-const cds = require("@sap/cds/lib");
-const NotificationsBuildPlugin = require("./lib/build");
+const cds = require("@sap/cds");
+const { copy, exists, path } = cds.utils
 
-if (cds.cli.command === "build") {
-  // register build plugin
-  cds.build?.register?.('notifications', NotificationsBuildPlugin);
-}
+cds.build?.register?.('notifications', class NotificationsBuildPlugin extends cds.build.Plugin {
+  static taskDefaults = { src: cds.env.folders.srv }
 
-else cds.once("served", async () => {
+  static hasTask() {
+    const notificationTypesFile = cds.env.requires?.notifications?.types;
+    return notificationTypesFile === undefined ? false : exists(notificationTypesFile);
+  }
+
+  async build() {
+    if (exists(cds.env.requires.notifications?.types)) {
+      const fileName = path.basename(cds.env.requires.notifications.types);
+      await copy(cds.env.requires.notifications.types).to(path.join(this.task.dest, fileName));
+    }
+  }
+});
+
+
+cds.once("served", async () => {
   const { validateNotificationTypes, readFile } = require("./lib/utils");
   const { createNotificationTypesMap } = require("./lib/notificationTypes");
   const production = cds.env.profiles?.includes("production");
